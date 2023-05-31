5b609b6a8dbf935396a11129e7a9b3a28d5662cc
Fabio Carlesso <fabio.carlesso@gmail.com>
2023-05-31 16:58:56 -0300

    feat: Create 2 strategies to pay (Paypal and CreditCard)

---
+++ src/main/java/com/fabiocarlesso/strategies/CreditCard.java
@@ -0,0 +1,22 @@
+package com.fabiocarlesso.strategies;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+public class CreditCard {
+    @Getter
+    @Setter
+    private int amount;
+    private String number;
+    private String date;
+    private String cvv;
+
+    CreditCard(String number, String date, String cvv) {
+        this.amount = 100_000;
+        this.number = number;
+        this.date = date;
+        this.cvv = cvv;
+    }
+}
