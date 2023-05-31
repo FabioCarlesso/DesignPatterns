93afff506823c44db11964d098e83f94fdc19893
Fabio Carlesso <fabio.carlesso@gmail.com>
2023-05-31 16:59:56 -0300

    feat: Orchestration order and main to pay something

---
+++ src/main/java/com/fabiocarlesso/order/Order.java
@@ -0,0 +1,25 @@
+package com.fabiocarlesso.order;
+
+import com.fabiocarlesso.strategies.PayStrategy;
+import lombok.Getter;
+import lombok.Setter;
+
+public class Order {
+    @Getter
+    private int totalCost = 0;
+    @Getter
+    private boolean isClosed = false;
+
+    public void processOrder(PayStrategy strategy) {
+        strategy.collectPaymentDetails();
+        // Here we could collect and store payment data from the strategy.
+    }
+
+    public void setTotalCost(int cost) {
+        this.totalCost += cost;
+    }
+
+    public void setClosed() {
+        isClosed = true;
+    }
+}
